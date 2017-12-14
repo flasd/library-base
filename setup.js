ed43b828745f1e7f9fcab16f33730c2a70c15302
Marcel O. Coelho <flasd2@gmail.com>
2017-12-14 18:01:22 -0200

    Added logic to add npm scripts to the final package file

--- setup.js
+++ setup.js
@@ -17,6 +17,17 @@ const devDependencies = [
     'webpack',
 ];
 
+const scripts = {
+    lint: 'cross-env NODE_ENV=test eslint src/**/*.js',
+    test: 'cross-env NODE_ENV=test nyc -x src/**/*.test.js mocha src/**/*.test.js --require babel-core/register',
+    build: 'npm test && npm run lint && cross-env NODE_ENV=production webpack',
+    report: 'nyc report -x ./src/index.spec.js --reporter=text-lcov | coveralls',
+};
+
+/**
+ *
+ * @param {string} path the path of the file/directory to be removed.
+ */
 function rm(path) {
     if (fs.existsSync(path)) {
         if (fs.lstatSync(path).isDirectory()) {
@@ -37,21 +48,33 @@ function rm(path) {
     }
 }
 
+// ##### Removes old files
 rm('./.git');
 rm('./package.json');
 rm('./package-lock.json');
 rm('./README.md');
 rm('./LICENSE');
 
+// ##### Create new README
 fs.writeFileSync('./README.md', '# your_library\n Here goes your library description!\n\0', 'utf8');
 
+// ##### Init new git repository
 childProcess.spawnSync('git', ['init'], { stdio: 'inherit' });
 process.stdout.write('\n');
 
+// ##### Init new npm repository
 childProcess.spawnSync('npm', ['init'], { stdio: 'inherit', detached: true });
 process.stdout.write('\n');
 
+// ##### Install devDependencies
 const installArgs = ['install'].concat(devDependencies).concat(['--save-dev']);
 childProcess.spawnSync('npm', installArgs, { stdio: 'inherit', detached: true });
 
+// ##### Update scripts
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+fs.unlinkSync('./package.json');
+pkg.scripts = scripts;
+fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 4));
+
+// ##### Unlink setup script
 rm('./setup.js');
