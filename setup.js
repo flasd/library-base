3f1337991e359c96e1dfb5f97c33e05223954c94
Marcel O. Coelho <flasd2@gmail.com>
2017-12-14 15:52:37 -0200

    Added the setup script

---
+++ setup.js
@@ -0,0 +1,30 @@
+const childProcess = require('child_process');
+const fs = require('fs');
+
+const devDependencies = [
+    'babel-cli',
+    'babel-core',
+    'babel-loader',
+    'babel-preset-env',
+    'chai',
+    'coveralls',
+    'cross-env',
+    'eslint',
+    'eslint-config-airbnb-base',
+    'eslint-plugin-import',
+    'mocha',
+    'nyc',
+    'webpack',
+];
+
+fs.unlinkSync('./.git');
+fs.unlinkSync('./package.json');
+fs.unlinkSync('./package-lock.json');
+fs.unlinkSync('./README.md');
+fs.unlinkSync('./LICENSE');
+
+fs.writeFileSync('./README.md', '# your_library\n Here goes your library description!', './README.md');
+
+childProcess.execSync('git init');
+childProcess.execSync('npm init');
+childProcess.execSync(`npm install ${devDependencies.join(' ')}`);
